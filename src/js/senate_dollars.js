203a2ea767a794c73f195ade3d1546ebba1db176
Rob Solomon <rob@imtc.gatech.edu>
2015-11-15 11:53:17 -0500

    Initial Commit of Project Code from Dropbox

---
+++ src/js/senate_dollars.js
@@ -0,0 +1,90 @@
+var chart;
+var height = 400;
+var width = 600;
+var margin = {
+    top: 20,
+    right: 70,
+    bottom: 30,
+    left: 60
+};
+
+
+//Gets called when the page is loaded.
+function init() {
+    treemap = d3.layout.treemap()
+        .size([width, height])
+        .sticky(true)
+        .value(function (d) {
+            return d.values;
+        });
+
+    div = d3.select("tmap").append("div")
+        .style("position", "relative")
+        .style("width", "300px")
+        .style("height", "100%")
+
+    //.style("width", (width + margin.left + margin.right) + "px")
+    //.style("height", (height + margin.top + margin.bottom) + "px")
+    //.style("left", margin.left + "px")
+    .style("top", margin.top + "px");
+
+    color_scale = d3.scale.category20c();
+
+    d3.csv('data/sample_data_org_aggr.csv', update);
+
+}
+
+//Called when the update button is clicked
+function updateClicked() {}
+
+//Callback for when data is loaded
+function update(rawdata) {
+    console.log("success");
+    nested_data = d3.nest()
+        .key(function (d) {
+            return d.DonorOrganization;
+        })
+        .rollup(function (leaves) {
+            return d3.sum(leaves, function (d) {
+                return d.total;
+            })
+        })
+        .entries(rawdata);
+
+    // Creat the root node for the treemap
+    var root = {};
+    root.children = nested_data;
+
+    //var nodes = treemap.nodes(root);
+
+    var node = div.datum(root).selectAll(".node")
+        .data(treemap.nodes)
+        .enter().append("div")
+        .attr("class", "node")
+        .call(position)
+        .style("background", function (d) {
+            return d.children ? color_scale(d.key) : null;
+        })
+        .text(function (d) {
+            return d.key;
+        });
+
+
+
+
+}
+
+function position() {
+    this.style("left", function (d) {
+            return d.x + "px";
+        })
+        .style("top", function (d) {
+            return d.y + "px";
+        })
+        .style("width", function (d) {
+            return Math.max(0, d.dx - 1) + "px";
+        })
+        .style("height", function (d) {
+            return Math.max(0, d.dy - 1) + "px";
+        });
+}
---
+++ src/script.js
@@ -0,0 +1,70 @@
+// JavaScript Document
+
+// declare a module
+var myAppModule = angular.module('myApp', []);
+
+//Module Loading and dependencies
+angular.module('myModule', ['ui.bootstrap']);
+
+
+//Initialize Pull Page Scroll Plugin
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        //Navigation
+        menu: '#menu',
+        lockAnchors: false,
+        anchors:['firstPage', 'secondPage'],
+        navigation: false,
+        navigationPosition: 'right',
+        navigationTooltips: ['firstSlide', 'secondSlide'],
+        showActiveTooltip: false,
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: true,
+        fitToSection: true,
+        fitToSectionDelay: 1000,
+        scrollBar: false,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        normalScrollElements: '#element1, .element2',
+        scrollOverflow: false,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
+
+        //Design
+        controlArrows: true,
+        verticalCentered: true,
+        resize : false,
+        sectionsColor : ['#ccc', '#fff'],
+        paddingTop: '3em',
+        paddingBottom: '10px',
+        fixedElements: '#header, .footer',
+        responsiveWidth: 0,
+        responsiveHeight: 0,
+
+        //Custom selectors
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+    });
+});
