39f9b2d0a073d9358f8aa49944009a10e46c48e0
Lisa Li <liza90319@hotmail.com>
2015-12-06 13:39:57 -0500

    delete AngularJS

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,1 @@
-var myApp = angular.module('myApp' , []);
-
-//Controller
-//place to put code associate with this controller
-//this is the controller for the view, any code inside of here will be controlling the html inside the sub-div with ng-controller name
-
-$(function(){
-	if($('li').text().indexOf('Alan') != -1){
-		 console.log("Has Alan");
-		
-	}
-});
+var app = angular.module('myApp', []);
